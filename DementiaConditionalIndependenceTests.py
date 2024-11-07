0e49c7a40e5aa6b5207191d6e12a6e2e2bf12178
Tarteel <127419053+TarteelA@users.noreply.github.com>
2024-11-07 15:19:32 +0000

    Update DementiaConditionalIndependenceTests.py

--- DementiaConditionalIndependenceTests.py
+++ DementiaConditionalIndependenceTests.py
@@ -1,29 +1,28 @@
+#Import Libraries
 import bnlearn as bn
 import pandas as pd
 
-# definition of directed acyclic graphs (predefined structures)
+#Definition Of Directed Acyclic Graphs (Predefined Structures)
 edges = [('CDR', 'Subject_ID'),('CDR', 'MRI_ID'),('CDR', 'Group'),('CDR', 'Visit'),('CDR', 'MR_Delay'),('CDR', 'M/F'),('CDR', 'Hand'),('CDR', 'Age'),('CDR', 'EDUC'),('CDR', 'SES'),('CDR', 'MMSE'),('CDR', 'eTIV'),('CDR', 'nWBV'),('CDR', 'ASF')]
 
-# examples of training data include 'data\lung_cancer-train.csv' or  'data\lang_detect_train.csv', etc.
-# examples of net structure (as below): edges_langdet1, edges_langdet2, edges_lungcancer1, edges_lungcancer2
-# choices of CI test: chi_square, g_sq, log_likelihood, freeman_tuckey, modified_log_likelihood, neyman, cressie_read
+#Declare Training Data, Network Structure, and Type Of Conditional Independence Test
 TRAINING_DATA = './data/dementia_data-MRI-features.csv'
 NETWORK_STRUCTURE = edges
 CONDITIONAL_INDEPENDENCE_TEST = 'cressie_read'
 
-# data loading using pandas
+#Data Loading Using Pandas
 data = pd.read_csv(TRAINING_DATA, encoding='UTF-8')
 print("DATA:\n", data)
 
-# creation of the directed acyclic graph (DAG)
+#Creation Of Directed Acyclic Graph (DAG)
 DAG = bn.make_DAG(NETWORK_STRUCTURE)
 print("DAG:\n", DAG)
 
-# parameter learning using Maximum Likelihood Estimation
+#Parameter Learning Using Maximum Likelihood Estimation
 model = bn.parameter_learning.fit(DAG, data, methodtype="maximumlikelihood")
 print("model=",model)
 
-# statististical test of independence
+#Statistical Test Of Independence
 model = bn.independence_test(model, data, test=CONDITIONAL_INDEPENDENCE_TEST, alpha=0.05)
 ci_results = list(model['independence_test']['stat_test'])
 num_edges2remove = ci_results.count(False)
